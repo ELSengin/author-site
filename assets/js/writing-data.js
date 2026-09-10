97a8813e1259bd31955cac383626dd5112ef307d
Erick Sokn <118176778+ELSengin@users.noreply.github.com>
2026-09-10 13:41:50 -0500

    Update writing-data.js

--- assets/js/writing-data.js
+++ assets/js/writing-data.js
@@ -63,7 +63,7 @@ window.WRITING_ITEMS = [
     section: "poetry",
     title: "Witches Brew",
     type: "Poem",
-    publication: "Independently published, 2026",
+    publication: "Independently published as an illustrated children's book for Halloween, 2026",
     internalUrl: "writing/witches-brew.html"
   },
   {
